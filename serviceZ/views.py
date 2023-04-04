455ec57524f24710ac07f6595fb7acdfdb82e46f
0x17 <jrespinoza.17@gmail.com>
2023-04-04 18:19:46 -0500

    Updated fkljgjhldfkjgh

--- serviceZ/views.py
+++ serviceZ/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(response):
-    return render(response,template_name="mainTest.html")#HttpResponse("Welcome to ServiceZ")
+    return render(response,template_name="homePage.html")#HttpResponse("Welcome to ServiceZ")
+
+def login(response):
+    return render(response,template_name="loginPage.html")#HttpResponse("Welcome to ServiceZ")
