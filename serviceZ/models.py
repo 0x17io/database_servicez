aadb80f335e43efc4c85881d2dd7bd300c793689
0x17 <jrespinoza.17@gmail.com>
2023-04-16 17:29:41 -0500

    Updated homePage title

--- serviceZ/models.py
+++ serviceZ/models.py
@@ -2,11 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Account (models.Model):
-    PersonID = models.IntegerField()
-    FirstName = models.CharField(max_length=250)
 
-    def __str__(self):
-        return f"{self.PersonID} is {self.FirstName}"
+class Account (models.Model):
+
+    # Main attributes
+    ID = models.IntegerField()
+    FirstName = models.CharField(max_length=250)
+    LastName = models.CharField(max_length=250)
+    EmailAddr = models.CharField(max_length=250)
+    Zipcode = models.IntegerField()
+    Language = models.CharField(max_length=250)
     class Meta:
         db_table = "Account"
