f65a09a610c132170d4c7a7cdd2c7af0f40d2bbc
0x17 <jrespinoza.17@gmail.com>
2023-04-16 17:45:25 -0500

    Updated added additional models

--- serviceZ/models.py
+++ serviceZ/models.py
@@ -14,3 +14,116 @@ class Account (models.Model):
     Language = models.CharField(max_length=250)
     class Meta:
         db_table = "Account"
+
+class Background (models.Model):
+
+    # Main attributes
+    ID = models.IntegerField()
+    HasCharges = models.BooleanField()
+    Charges = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = "Background"
+
+class AdminRoles (models.Model):
+
+    # main attributes
+    RoleID = models.IntegerField()
+    Type = models.CharField(max_length=250)
+    Description = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = "AdminRoles"
+
+class Administrator (models.Model):
+
+    # Main attributes
+    ID = models.IntegerField()
+    RoleID = models.IntegerField()
+
+    class Meta:
+        db_table = "Administrator"
+
+class Clients (models.Model):
+
+    # Main attributes
+    ID = models.IntegerField()
+    ClientID = models.IntegerField()
+
+    class Meta:
+        db_table = "Clients"
+
+class Contractors (models.Model):
+
+    # Main attributes
+    ID = models.IntegerField()
+    ContractorID = models.IntegerField()
+    ServiceID = models.IntegerField()
+    Availability = models.BooleanField()
+
+    class Meta:
+        db_table = "Contractors"
+
+
+class Services (models.Model):
+    # Main attributes
+    ServiceID = models.IntegerField()
+    TypeID = models.IntegerField()
+    Description = models.CharField(max_length=250)
+    Rate = models.FloatField()
+
+    class Meta:
+        db_table = "Services"
+
+class ServiceTypes (models.Model):
+    # Main attributes
+    TypeID = models.IntegerField()
+    Type = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = "ServiceTypes"
+
+class Requests (models.Model):
+    # Main attributes
+    RequestID = models.IntegerField()
+    ClientID = models.IntegerField()
+    ServiceID = models.IntegerField()
+    Timestamp = models.DateTimeField()
+
+    class Meta:
+        db_table = "Requests"
+
+
+class Reviews (models.Model):
+    # Main attributes
+    ReviewID = models.IntegerField()
+    ClientID = models.IntegerField()
+    ContractorID = models.IntegerField()
+    Rating = models.IntegerField()
+    Comment = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = "Reviews"
+
+
+class Transactions (models.Model):
+    # Main attributes
+    TransID = models.IntegerField()
+    ClientID = models.IntegerField()
+    ContractorID = models.IntegerField()
+    AmountPaid = models.FloatField()
+    Timestamp = models.DateTimeField()
+
+    class Meta:
+        db_table = "Transactions"
+
+class Orders (models.Model):
+    # Main attributes
+    OrderID = models.IntegerField()
+    ClientID = models.IntegerField()
+    ContractorID = models.IntegerField()
+    TransactionID = models.IntegerField()
+    Timestamp = models.DateTimeField()
+
+    class Meta:
+        db_table = "Orders"
