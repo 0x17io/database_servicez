d705ed12903d28edf9512fc279e6b0b081f5aa6d
0x17 <jrespinoza.17@gmail.com>
2023-05-07 15:37:44 -0500

    Update - added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='database_servicez',
+    version='',
+    packages=['serviceZ', 'serviceZ.migrations', 'database_servicez'],
+    url='',
+    license='',
+    author='ruben espinoza',
+    author_email='',
+    description='Database django set up'
+)
